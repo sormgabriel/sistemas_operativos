e5efbda6d2f5a0344c3341addee5d2dbd76f1ec2
Gabriel <sormgabriel@gmail.com>
2015-09-27 02:54:06 -0300

    process_test test01 added

--- test/process_test.py
+++ test/process_test.py
@@ -1,7 +1,22 @@
 class Process_test(unittest.TestCase):
+	
+	@classmethod
+	def setUpClass(cls):
+		pass
+	
 
-    def test1():
-        pass
+	def test1_a(self):
+		process= Process new(program_counter,registers,variables)
+		pc= process._program_counter()
+		cpu.load(process)
+		assertEqual(process._program_counter(),cpu._program_counter())
+		cpu.run_process()
+		assertNotEqual(pc, cpu._program_counter())
+		assertEqual(process._program_counter(), cpu._program_counter())
+
+
+    	def test1():
+        	pass
 
 
 
